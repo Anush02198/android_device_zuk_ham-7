39919375529c1cddc0945428e74bbd4fa54dcfd5
Anush12133 <Anush.4376@gmail.com>
2018-06-19 14:26:22 +0000

    Update to Radio HAL V1.1

--- ham.mk
+++ ham.mk
@@ -211,6 +211,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_treble=true \
     persist.camera.4k2k.enable=1
 
+#Radio
+PRODUCT_PACKAGES += \
+	android.hardware.radio@1.1-impl \
+
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
